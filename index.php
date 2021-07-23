cbe04b89d3c037231d0ae52ed3fdf8fdca690fe8
ekabagusfernadi <bagusfernadieka@gmail.com>
2021-07-23 11:52:05 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -9,5 +9,9 @@
 <body>
     <h1>Hello World!</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus laborum culpa quibusdam veniam corporis quasi tenetur sint, esse deleniti ex consequatur tempora maiores similique officia, quos illo. Tempora, distinctio accusantium.</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
